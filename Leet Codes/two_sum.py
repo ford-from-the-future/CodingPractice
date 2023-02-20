ef16d2cfb447e65c4704424cffa43538571e362d
ford-from-the-future <khalic44@gmail.com>
2023-02-19 20:55:55 -0500

    update two sum file with instructions

--- Leet Codes/two_sum.py
+++ Leet Codes/two_sum.py
@@ -1,2 +1,10 @@
+#Leet Code Question, Two Sum Solution
+#Composed by Khali A. Crawford on 02/19/2023 @ 8:49 PM
+
+#Instructions
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
